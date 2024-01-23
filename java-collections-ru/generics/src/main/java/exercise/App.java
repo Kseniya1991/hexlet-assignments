84717cae877f9eebf430c5c475d24abd9b63184a
@hexlet/cli <support@hexlet.io>
2024-01-24 02:03:18 +0500

    submit java-collections-ru/generics

---
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -0,0 +1,45 @@
+package exercise;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+        public class App {
+
+            public static List<Map<String, String>> findWhere(List<Map<String, String>> listOfBooks, Map<String, String> strings) {
+                if (listOfBooks.isEmpty() || strings.isEmpty()) {
+                    return new ArrayList<>();
+                }
+                List<Map<String, String>> coincidenceOfNames = new ArrayList<>();
+
+                for (Map<String, String> book : listOfBooks) {
+                    for (Map.Entry<String, String> book2 : strings.entrySet()) {
+                        if (book.get("title").equals(book2.getValue()) ||
+                                book.get("author").equals(book2.getValue()) ||
+                                book.get("year").equals(book2.getValue())) {
+                            if (!coincidenceOfNames.contains(book)) {
+                                coincidenceOfNames.add(book);
+                            }
+                        } 
+                    }
+                }
+            return coincidenceOfNames;
+            }
+
+            public static void main(String[] args) {
+                List<Map<String, String>> BOOKS = List.of(
+                        Map.of("title", "Cymbeline", "author", "Shakespeare", "year", "1611"),
+                        Map.of("title", "Book of Fooos", "author", "FooBar", "year", "1111"),
+                        Map.of("title", "The Tempest", "author", "Shakespeare", "year", "1611"),
+                        Map.of("title", "Book of Foos Barrrs", "author", "FooBar", "year", "2222"),
+                        Map.of("title", "Still foooing", "author", "FooBar", "year", "3333"),
+                        Map.of("title", "Happy Foo", "author", "FooBar", "year", "4444")
+                );
+
+                Map<String, String> where = Map.of("title", "Still foooing",
+                        "author", "FooBar",
+                        "year", "3333"
+                );
+                List<Map<String, String>> actual = App.findWhere(BOOKS, where);
+                System.out.println("результат\n" + actual + "\n размер - " + actual.size());
+            }
+        }
