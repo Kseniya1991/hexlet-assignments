16aa02a9064cf566d468e84d4283bba116713f8e
@hexlet/cli <support@hexlet.io>
2024-01-24 13:02:59 +0500

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -23,22 +23,4 @@ import java.util.Map.Entry;
                 }
                 return coincidenceOfNames;
             }
-
-            public static void main(String[] args) {
-                List<Map<String, String>> BOOKS = List.of(
-                        Map.of("title", "Cymbeline", "author", "Shakespeare", "year", "1611"),
-                        Map.of("title", "Book of Fooos", "author", "FooBar", "year", "1111"),
-                        Map.of("title", "The Tempest", "author", "Shakespeare", "year", "1611"),
-                        Map.of("title", "Book of Foos Barrrs", "author", "FooBar", "year", "2222"),
-                        Map.of("title", "Still foooing", "author", "FooBar", "year", "3333"),
-                        Map.of("title", "Happy Foo", "author", "FooBar", "year", "4444")
-                );
-
-                Map<String, String> where = Map.of("title", "Still foooing",
-                        "author", "FooBar",
-                        "year", "3333"
-                );
-                List<Map<String, String>> actual = App.findWhere(BOOKS, where);
-                System.out.println("результат\n" + actual + "\n размер - " + actual.size());
-            }
         }
