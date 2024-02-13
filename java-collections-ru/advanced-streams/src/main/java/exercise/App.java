c4d44fc133965044b11ef48ac77b0ce279ce659b
@hexlet/cli <support@hexlet.io>
2024-02-13 12:54:53 +0500

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,16 +1,29 @@
 package exercise;
 
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
     public class App {
         public static String getForwardedVariables(String conf) {
             String[] words = conf.split(","); //строку в массив
-            var result =  Arrays.stream(words)
+            var result1 =  Arrays.stream(words)
                     .filter(word -> word.startsWith("X_FORWARDED_"))
                     .map(word -> word.replace("X_FORWARDED_",  "").trim())
                     .collect(Collectors.toList());
-            return result.toString();
+            String string = "";
+            for (String str : result1) {
+                if(string.equals("")) {
+                    string += str;
+                } else {
+                    string += "," + str;
+                }
+            }
+            return string;
+        }
+
+        public static void main(String[] args) {
+            String result1 = "X_FORWARDED_mail=tirion@google.com,X_FORWARDED_HOME=/home/tirion,language=en";
+            String expected1 = App.getForwardedVariables(result1);  //"variable=value";
+            System.out.println(expected1);
         }
 }
