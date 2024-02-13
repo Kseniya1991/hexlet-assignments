7b02f1d73775f2110d3df2e0c43ca81a3c738f05
@hexlet/cli <support@hexlet.io>
2024-02-13 13:31:23 +0500

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -1,29 +1,21 @@
 package exercise;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
-    public class App {
-        public static String getForwardedVariables(String conf) {
-            String[] words = conf.split(","); //строку в массив
-            var result1 =  Arrays.stream(words)
-                    .filter(word -> word.startsWith("X_FORWARDED_"))
-                    .map(word -> word.replace("X_FORWARDED_",  "").trim())
-                    .collect(Collectors.toList());
-            String string = "";
-            for (String str : result1) {
-                if(string.equals("")) {
-                    string += str;
-                } else {
-                    string += "," + str;
-                }
-            }
-            return string;
-        }
+class App {
+    public static String getForwardedVariables(String content) {
+        return Arrays.stream(content.split("\n"))
+                .filter(line -> line.startsWith("environment="))
+                .map(line -> line.replaceAll("environment=", ""))
+                .map(line -> line.replaceAll("\"", ""))
+                .map(line -> Arrays.stream(line.split(",")))
+                .flatMap(line -> line.filter(subline -> subline.startsWith("X_FORWARDED_")))
+                .map(line -> line.replaceAll("X_FORWARDED_", ""))
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+    }
 
-        public static void main(String[] args) {
-            String result1 = "X_FORWARDED_mail=tirion@google.com,X_FORWARDED_HOME=/home/tirion,language=en";
-            String expected1 = App.getForwardedVariables(result1);  //"variable=value";
-            System.out.println(expected1);
-        }
 }
