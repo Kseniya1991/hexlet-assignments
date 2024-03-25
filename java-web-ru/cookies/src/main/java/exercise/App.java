333081df2c2c3a18f71890976b89fb31cf2648fb
@hexlet/cli <support@hexlet.io>
2024-03-25 14:23:00 +0500

    submit java-web-ru/cookies

--- java-web-ru/cookies/src/main/java/exercise/App.java
+++ java-web-ru/cookies/src/main/java/exercise/App.java
@@ -15,8 +15,8 @@ public final class App {
         });
 
         // BEGIN
-        app.post(NamedRoutes.usersPath(), UsersController::create);
         app.get(NamedRoutes.buildUserPath(), UsersController::build);
+        app.post(NamedRoutes.usersPath(), UsersController::create);
         // END
 
         app.get(NamedRoutes.userPath("{id}"), UsersController::show);
