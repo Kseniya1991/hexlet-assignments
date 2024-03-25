f8df9bf62dd7d2b86d16d0afddc620331a4f3bef
Kseniya1991 <94966269+Kseniya1991@users.noreply.github.com>
2024-03-25 17:30:44 +0500

    Update SessionsController.java

--- java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -11,6 +11,35 @@ import io.javalin.http.Context;
 public class SessionsController {
 
     // BEGIN
-    
+    public static void index(Context ctx) {
+        var page = new MainPage(ctx.sessionAttribute("user"));
+        ctx.render("index.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void build(Context ctx) {
+        var page = new LoginPage("", "");
+        ctx.render("build.jte", Collections.singletonMap("page", page));
+    }
+
+    public static void create(Context ctx) {
+        var name = ctx.formParam("name");
+        var password = ctx.formParam("password");
+
+        var user = UsersRepository.findByName(name);
+
+        if (user != null && user.getPassword().equals(encrypt(password))) {
+            ctx.sessionAttribute("user", user.getName());
+            ctx.redirect("/");
+        } else {
+            var errorMessage = "Wrong username or password";
+            var page = new LoginPage(name, errorMessage);
+            ctx.render("build.jte", Collections.singletonMap("page", page));
+        }
+    }
+
+    public static void destroy(Context ctx) {
+        ctx.sessionAttribute("user", null);
+        ctx.redirect(NamedRoutes.rootPath());
+    }
     // END
 }
