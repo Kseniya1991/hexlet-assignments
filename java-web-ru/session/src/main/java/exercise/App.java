9331dbcafc3a67c1cd296ca30c38a5e9740e674b
@hexlet/cli <support@hexlet.io>
2024-03-25 17:14:08 +0500

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/App.java
+++ java-web-ru/session/src/main/java/exercise/App.java
@@ -3,7 +3,6 @@ package exercise;
 import io.javalin.Javalin;
 import exercise.controller.SessionsController;
 import exercise.util.NamedRoutes;
-import io.javalin.rendering.template.JavalinJte;
 
 
 public final class App {
@@ -11,15 +10,19 @@ public final class App {
     public static Javalin getApp() {
 
         var app = Javalin.create(config -> {
-            config.bundledPlugins.enableDevLogging();
-            config.fileRenderer(new JavalinJte());
+            config.plugins.enableDevLogging();
         });
 
         // BEGIN
-        app.get(NamedRoutes.rootPath(), SessionsController::show);
+//        app.get(NamedRoutes.rootPath(), ctx -> {
+//           ctx.render("index.jte");
+////            ctx.redirect(NamedRoutes.buildSessionPath());
+//        });
         app.get(NamedRoutes.buildSessionPath(), SessionsController::build);
-        app.post(NamedRoutes.loginPath(), SessionsController::create);
-        app.post(NamedRoutes.logoutPath(), SessionsController::delete);
+        app.post(NamedRoutes.loginPath(),SessionsController::enter);
+        app.get(NamedRoutes.rootPath(),SessionsController::show);
+        app.get(NamedRoutes.loginPath(), SessionsController::show);
+        app.post(NamedRoutes.logoutPath(), SessionsController::destroy);
         // END
 
         return app;
--- java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
+++ java-web-ru/session/src/main/java/exercise/controller/SessionsController.java
@@ -1,52 +1,57 @@
 package exercise.controller;
 
-
+import java.util.Collections;
 import exercise.dto.MainPage;
 import exercise.dto.LoginPage;
 import exercise.repository.UsersRepository;
 import static exercise.util.Security.encrypt;
 
+import exercise.util.NamedRoutes;
 import io.javalin.http.Context;
-
-import java.util.Collections;
 
 public class SessionsController {
 
     // BEGIN
     public static void build(Context ctx) {
-        var page = new LoginPage();
+        var page = new LoginPage("","");
         ctx.render("build.jte", Collections.singletonMap("page", page));
+    }
+    public static void enter(Context ctx) {
+        var login = ctx.formParam("name");
+        var password = encrypt(ctx.formParam("password"));
+        var user = UsersRepository.findByName(login);
+        var userPassword = "";
+        if (user != null) {
+            userPassword = user.getPassword();
+        } else {
+            userPassword = "";
+        }
+        if (password.equals(userPassword) && user != null) {
+            ctx.sessionAttribute("authorized", String.valueOf(true));
+            ctx.sessionAttribute("name", login);
+            ctx.redirect(NamedRoutes.loginPath());
+        } else {
+            var error = "Wrong username or password";
+            var page = new LoginPage(login, error);
+            ctx.render("build.jte", Collections.singletonMap("page", page));
+        }
+
     }
 
     public static void show(Context ctx) {
-        var page = new MainPage(ctx.sessionAttribute("name"));
-        ctx.render("index.jte", Collections.singletonMap("page", page));
-    }
-
-    public static void create(Context ctx) {
-        var name = ctx.formParam("name");
-        var password = Security.encrypt(ctx.formParam("password"));
-        //Проверка имени и пароля:
-        var user = UsersRepository.findByName(name);
-        if (user != null) {
-            if (user.getPassword().equals(password)) {
-                ctx.sessionAttribute("name", name);
-                ctx.redirect(NamedRoutes.rootPath());
-            }
-            else {
-                var page = new LoginPage(name, "Wrong username or password");
-                ctx.render("build.jte", Collections.singletonMap("page", page));
-            }
-        }
-        else {
-            var page = new LoginPage(name, "Wrong username or password");
-            ctx.render("build.jte", Collections.singletonMap("page", page));
+        var authorized = ctx.sessionAttribute("authorized");
+        if (authorized != null) {
+            var name = ctx.sessionAttribute("name");
+            var page = new MainPage(name);
+            ctx.render("index.jte", Collections.singletonMap("page", page));
+        } else {
+            var page = new MainPage("");
+            ctx.render("index.jte", Collections.singletonMap("page", page));
         }
     }
-
-    public static void delete(Context ctx) {
-        ctx.sessionAttribute("name", null);
-        ctx.redirect(NamedRoutes.rootPath());
+    public static void destroy(Context ctx) {
+        ctx.sessionAttribute("authorized", null);
+        ctx.redirect(NamedRoutes.loginPath());
     }
     // END
 }
