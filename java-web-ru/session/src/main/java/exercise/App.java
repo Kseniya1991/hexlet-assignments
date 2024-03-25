8d8fc4768a50c70651b38bb53e6178f680e01698
@hexlet/cli <support@hexlet.io>
2024-03-25 17:56:00 +0500

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import io.javalin.Javalin;
+import exercise.controller.SessionsController;
+import exercise.util.NamedRoutes;
+
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        // BEGIN
+        app.get(NamedRoutes.rootPath(), SessionsController::index);
+        app.get(NamedRoutes.buildSessionPath(), SessionsController::build);
+        app.post(NamedRoutes.loginPath(), SessionsController::create);
+        app.post(NamedRoutes.logoutPath(), SessionsController::destroy);
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
